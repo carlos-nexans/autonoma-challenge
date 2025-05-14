02f67a0ca9ef2fe770f1ab99756bdb9c058d5d40
Carlos Nexans <carlos.nexans@clau.com>
2025-05-13 21:56:36 -0300

    refine thread functionality when deleting

---
+++ apps/web/app/chat/[id]/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider
+} from "@/components/ui/sidebar";
+import React from "react";
+
+export default function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ id: string }> 
+}>) {
+  const { id } = React.use(params)
+  return (
+    <SidebarProvider>
+    <AppSidebar currentThread={id} />
+    <SidebarInset className="bg-gray-50">
+      {children}
+    </SidebarInset>
+  </SidebarProvider>
+  );
+}
